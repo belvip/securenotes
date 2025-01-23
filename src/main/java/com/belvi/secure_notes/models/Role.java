8f8cf7ffd8f558701dc78e3c6747dfe24cad35d9
belvip <pouadjeubelvi@gmail.com>
2025-01-23 20:35:28 +0100

    Defining UserDetailsServiceImpl ansd UserDetailsImpl

--- src/main/java/com/belvi/secure_notes/models/Role.java
+++ src/main/java/com/belvi/secure_notes/models/Role.java
@@ -2,11 +2,19 @@ package com.belvi.secure_notes.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
 public class Role{
 
     @Id
---
+++ src/main/java/com/belvi/secure_notes/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.belvi.secure_notes.security.services;
+
+import com.belvi.secure_notes.models.User;
+import com.belvi.secure_notes.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+
+
+}
