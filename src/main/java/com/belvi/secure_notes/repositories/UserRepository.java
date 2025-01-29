ca569a49c9928190072efeae6b280b130ac3cb60
belvip <pouadjeubelvi@gmail.com>
2025-01-29 14:17:22 +0100

    The signup endpoint

--- src/main/java/com/belvi/secure_notes/repositories/UserRepository.java
+++ src/main/java/com/belvi/secure_notes/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.belvi.secure_notes.repositories;
 
 
 import com.belvi.secure_notes.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +15,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String username);
 
     Boolean existsByUserName(String username);
+
+    Boolean existsByEmail(String email);
+
+//   Boolean existsByEmail(@NotBlank @Size(max = 50) @Email String email);
 }
 
---
+++ src/main/java/com/belvi/secure_notes/security/response/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.belvi.secure_notes.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+}
